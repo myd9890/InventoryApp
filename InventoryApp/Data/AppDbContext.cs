d6b978c05b7aa7981259b8bc2b4951267bb29c6d
Manuja <manuja@gmail.com>
2025-09-11 16:56:23 +0530

    initial project setup

---
+++ InventoryApp/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using InventoryApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
+
---
+++ InventoryApp/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InventoryApp.Models
+{
+    public class Product
+    {
+        [Required(ErrorMessage = "Product ID is required.")]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Product name is required.")]
+        public required string Name { get; set; }
+        [Required(ErrorMessage = "Price is required.")]
+        public required decimal Price { get; set; }
+        [Required(ErrorMessage = "Stock quantity is required.")]
+        public required int Quantity { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
